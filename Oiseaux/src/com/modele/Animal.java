833bee8678a3ece96327a3ceef1e62e6e7c4a0fd
Fabrice06 <info@fabrice-fernandez.com>
2015-03-05 16:08:54 +0100

    fab

--- Oiseaux/src/com/modele/Animal.java
+++ Oiseaux/src/com/modele/Animal.java
@@ -1,25 +1,48 @@
 package com.modele;
 
+import java.awt.List;
 import java.util.ArrayList;
 import java.util.Date;
 
 public abstract class Animal {
 	//Attributs
 	double dureeDeVie;
-	Date dateDeNaissance; //Sera mis à jour par la méthode implémenté de l'interface reproduction
+	Date dateDeNaissance; //Sera mis ï¿½ jour par la mï¿½thode implï¿½mentï¿½ de l'interface reproduction
 	double coordoneeX;  
 	double coordoneeY;
+	
 	Animal pere;
 	Animal mere;
-	Sexe sexe;  //De manière à gérer le sexe aléatoirement par un mathRandom() par la méthode implémenté de l'interface reproduction
+	ArrayList<Animal> enfants;
+	
+	Sexe sexe;  //De maniï¿½re ï¿½ gï¿½rer le sexe alï¿½atoirement par un mathRandom() par la mï¿½thode implï¿½mentï¿½ de l'interface reproduction
 	//ArrayList <Animal> enfants;
 	
-	//Méthodes
+	//Mï¿½thodes
 	
 	abstract void deplacement();
 	
 	abstract double calculDureeDeVie();
 	
+	protected boolean isParents(Animal b){
+		return (this.pere == b || this.mere == b);	
+	}
 	
+	protected void setParents(Animal papa, Animal maman){
+		this.pere = papa;
+		this.mere= maman;
+	}
+	
+	protected void setChildren(Animal a){
+		this.enfants.add(a);
+	}
+	
+	public boolean isChild(Animal b){
+		return (this.enfants.contains(b));	
+	}
+	
+	public boolean isBrother(Animal b){
+		return (this.enfants.contains(b));	
+	}
 	
 }
