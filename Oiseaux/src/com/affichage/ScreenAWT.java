3e6375705aa00f58c4362eeb59accd44f8b21b26
Loha <t.loharano@gmail.com>
2015-03-11 12:16:25 +0100

    definition screen par defaut

--- Oiseaux/src/com/affichage/ScreenAWT.java
+++ Oiseaux/src/com/affichage/ScreenAWT.java
@@ -1,5 +1,29 @@
 package com.affichage;
 
-public class ScreenAWT {
+import java.awt.Color;
+import java.awt.FlowLayout;
 
+import javax.swing.JFrame;
+
+class ScreenAWT extends JFrame {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public static int screenX= 600;
+	public static int screenY = 800;
+	
+	public ScreenAWT(){
+		setSize(screenY, screenX);
+		setTitle("La vie des Oiseaux");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.getContentPane().setBackground(Color.decode("#B0E4E8"));
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new ScreenAWT();
+	}	
 }
